488883532562b573eb149f0738f3d41531f82597
Eldos <eldos.baktybekov@gmail.com>
2024-02-17 15:05:17 +0600

    :lock: Secure parsers

--- src/parsers.py
+++ src/parsers.py
@@ -18,10 +18,12 @@ def parse_sign_in_oidc_form_data(
 ) -> SignInOidcFormData:
     soup = BeautifulSoup(sign_in_oidc_form_html, 'lxml')
 
-    code = soup.find(attrs={'name': 'code'}).get('value')
-    scope = soup.find(attrs={'name': 'scope'}).get('value')
-    state = soup.find(attrs={'name': 'state'}).get('value')
-    session_state = soup.find(attrs={'name': 'session_state'}).get('value')
+    code = (soup.find(attrs={'name': 'code'}) or {}).get('value')
+    scope = (soup.find(attrs={'name': 'scope'}) or {}).get('value')
+    state = (soup.find(attrs={'name': 'state'}) or {}).get('value')
+    session_state = (
+        (soup.find(attrs={'name': 'session_state'}) or {}).get('value')
+    )
 
     return SignInOidcFormData(
         code=code,
@@ -39,9 +41,10 @@ def parse_account_login_form_data(
 ) -> AccountLoginFormData:
     soup = BeautifulSoup(account_login_form_html, 'lxml')
 
-    return_url = soup.find(attrs={'name': 'returnUrl'}).get('value')
+    return_url = (soup.find(attrs={'name': 'returnUrl'}) or {}).get('value')
     request_verification_token = (
-        soup.find(attrs={'name': '__RequestVerificationToken'}).get('value')
+        (soup.find(attrs={'name': '__RequestVerificationToken'}) or {})
+        .get('value')
     )
 
     return AccountLoginFormData(
@@ -61,17 +64,25 @@ def parse_connect_authorize_form_data(
 ) -> ConnectAuthorizeFormData:
     soup = BeautifulSoup(connect_authorize_form_html, 'lxml')
 
-    client_id = soup.find(attrs={'name': 'client_id'}).get('value')
-    redirect_uri = soup.find(attrs={'name': 'redirect_uri'}).get('value')
-    response_type = soup.find(attrs={'name': 'response_type'}).get('value')
-    scope = soup.find(attrs={'name': 'scope'}).get('value')
-    code_challenge = soup.find(attrs={'name': 'code_challenge'}).get('value')
-    code_challenge_method = (
-        soup.find(attrs={'name': 'code_challenge_method'}).get('value')
+    client_id = (soup.find(attrs={'name': 'client_id'}) or {}).get('value')
+    redirect_uri = (
+        (soup.find(attrs={'name': 'redirect_uri'}) or {}).get('value')
     )
-    response_mode = soup.find(attrs={'name': 'response_mode'}).get('value')
-    nonce = soup.find(attrs={'name': 'nonce'}).get('value')
-    state = soup.find(attrs={'name': 'state'}).get('value')
+    response_type = (
+        (soup.find(attrs={'name': 'response_type'}) or {}).get('value')
+    )
+    scope = (soup.find(attrs={'name': 'scope'}) or {}).get('value')
+    code_challenge = (
+        (soup.find(attrs={'name': 'code_challenge'}) or {}).get('value')
+    )
+    code_challenge_method = (
+        (soup.find(attrs={'name': 'code_challenge_method'}) or {}).get('value')
+    )
+    response_mode = (
+        (soup.find(attrs={'name': 'response_mode'}) or {}).get('value')
+    )
+    nonce = (soup.find(attrs={'name': 'nonce'}) or {}).get('value')
+    state = (soup.find(attrs={'name': 'state'}) or {}).get('value')
 
     return ConnectAuthorizeFormData(
         client_id=client_id,

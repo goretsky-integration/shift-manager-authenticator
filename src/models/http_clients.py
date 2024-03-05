01c678e77c8355ad8a436564cc7de7a5e3484b66
Eldos <eldos.baktybekov@gmail.com>
2024-03-05 11:51:25 +0600

    :label: Add new type

--- src/models/http_clients.py
+++ src/models/http_clients.py
@@ -2,7 +2,14 @@ from typing import NewType
 
 import httpx
 
-__all__ = ('AuthHttpClient', 'ShiftManagerHttpClient')
+__all__ = (
+    'AuthHttpClient',
+    'ShiftManagerHttpClient',
+    'AuthCredentialsStorageApiConnectionHttpClient',
+)
 
 AuthHttpClient = NewType('AuthHttpClient', httpx.Client)
 ShiftManagerHttpClient = NewType('ShiftManagerHttpClient', httpx.Client)
+AuthCredentialsStorageApiConnectionHttpClient = (
+    NewType('AuthCredentialsStorageApiConnectionHttpClient', httpx.Client)
+)
